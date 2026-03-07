9686ecc3bccce896844d06e769d227a6c6f836ef
hkayy33 <hkayy33@gmail.com>
2026-03-07 06:44:20 +0000

    Input section implementation

--- bespoke-dua-client/src/app/components/input-section/input-section.ts
+++ bespoke-dua-client/src/app/components/input-section/input-section.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-section',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './input-section.html',
   styleUrl: './input-section.scss',
 })
 export class InputSection {
+  input = '';
+
+  submitDua(value : string){
+    this.input = value;
+    console.log(this.input);
+    this.input = '';
+  }
 
 }
